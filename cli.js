29946596ff4b088c53391e25466f7a289efe28ad
John Flesch <john@fles.ch>
2017-05-01 12:02:54 -0500

    Adds the --verbose option to print errors

--- cli.js
+++ cli.js
@@ -13,17 +13,26 @@ const frank = meow(`
 
   Options
     --labels, -l  A comma separated list of issue labels.
+    --verbose     Print errors instead of failing silently.
 
 	Examples
-	  $ frankenstein flesch/graphql-frankenstein --labels "#frankenstein"
+    $ frankenstein flesch/graphql-frankenstein --labels "#frankenstein"
     $ frank flesch/graphql-frankenstein -l frankenstein,publish
     $ frankenstein flesch/graphql-frankenstein -l publish > schema.graphql
 
-`, { alias: { r:'repo', l:'labels' } });
+`, { alias: { r:'repo', l:'labels', v:'verbose' } });
 
 const [ repo ] = frank.input;
-const { labels = '' } = frank.flags;
+const { labels = '', verbose = false } = frank.flags;
 
 if (repo) {
-  frankenstein(repo, labels.split(',')).then(schema => console.log(printSchema(schema))).catch(e => console.log());
+  frankenstein(repo, labels.split(',')).then(schema => console.log(printSchema(schema))).catch(e => {
+    if (verbose) {
+      console.log(e);
+    }
+  });
+} else {
+  if (verbose) {
+    console.error(`Error: Repository is not defined!`);
+  }
 }
